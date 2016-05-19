1fe43a45502bddfe1c66c800064492c2f30548f5
Dasgupta, Smarajit <Smarajit.Dasgupta@news.com.au>
2016-05-19 12:49:00 +1000

    changes for wiki news

--- index.js
+++ index.js
@@ -10,42 +10,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 9001));
 
 app.get('/', function(req, res){
-  res.send('This works fine too!!');
+  res.send('Wiki news works fine!!');
 });
 
 app.post('/post', function(req, res){
   var parsed_url = url.format({
-    pathname: 'https://en.wikipedia.org/w/api.php',
+    pathname: 'http://wiki.news.com.au/rest/prototype/1/search/name.json',
     query: {
-      action: 'query',
-      prop: 'extracts',
-      exintro: '',
-      explaintext: '',
-      prop: 'extracts',
-      format: 'json', //json format
-      titles: req.body.text // search query
+      query: req.body.text // search query
     }
   });
 
   request(parsed_url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body);
-      var obj = data.query.pages;
+      var obj = data.group;
 
-      var first_page = obj[Object.keys(obj)[0]];
-      var first_snippet = first_page.extract.substring(0,250)+'...';
-      var result_url = 'http://en.wikipedia.org/wiki/' + first_page.title;
+      var first_item = obj[Object.keys(obj)[0]];
+     // var first_snippet = first_page.extract.substring(0,250)+'...';
+      var first_title = first_item.result[0].title;
+      var first_creator = (first_item.result[0].creator ? first_item.result[0].creator.displayName : "Anonymous")
 
-      var return_result =  result_url + " " + first_snippet;
+    //  var result_url = 'http://en.wikipedia.org/wiki/' + first_page.title;
 
+  
       var body = {
         response_type: "in_channel",
-        text: "Wikipedia says...",
+        text: "Found something on the wiki...",
         attachments: [
         {
-            title: first_page.title,
-            title_link: result_url,
-            text: first_snippet
+            title: first_title,
+         //   title_link: result_url,
+            text: first_creator
         }
     ]
       };
